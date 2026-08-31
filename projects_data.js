52b01a5a84bdf023400b5872b3adb37c1f246889
Lubabalo Mbulawa <lmbulawa4@gmail.com>
2026-08-31 17:58:01 +0200

    updated the form for contact section. now it is working

--- projects_data.js
+++ projects_data.js
@@ -1,7 +1,7 @@
 export const projects = [
     {
         title: "City of Cape Town Notification Dashboard",
-        description: "A brief description of the project.",
+        description: "This project implements an ETL (Extract, Transform, Load) pipeline for service request data in the City of Cape Town. The pipeline extracts data from a CSV file, transforms it into a suitable format, and loads it into a PostgreSQL database. The project also includes a dashboard that visualizes the service request data, providing insights into the city's service delivery performance.",
         video: "video/capeTownDashboard.mp4",
         technologies: ["JavaScript", "Python", "HTML", "CSS"],
         liveLink: "https://mbulawa-lubabalo.github.io/City-of-Cape-Town-Dashboard/",
@@ -9,7 +9,7 @@ export const projects = [
     },
     {
         title: "Supermarket",
-        description: "A brief description of the project.",
+        description: "A lightweight Java-based REST API that connects to a Supabase database to manage products. Built using the Java Standard Library (HttpServer, HttpClient) and Jackson for JSON handling.",
         video: "video/supermarket.mp4",
         technologies: ["Java", "JavaScript" , "HTML" , "CSS"],
         liveLink: "#",
